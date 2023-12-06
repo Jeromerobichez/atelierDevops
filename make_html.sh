27483b6f4add93fd62dab4ac39acd870fb6f0ab3
Jerome robichez <73989955+Jeromerobichez@users.noreply.github.com>
2023-12-06 15:53:55 +0100

    Update make_html.sh

--- make_html.sh
+++ make_html.sh
@@ -2,5 +2,5 @@
 
 EXE="${1:-FlatBiblio}"
 
-cat data/_liste.txt data/fg67hIOrf.txt | $EXE > wwwroot/fg67hIOrf.html
-cat data/_liste.txt data/rt25TYuy0.txt | $EXE > wwwroot/rt25TYuy0.html
+cat data/_liste.txt data/fg67hIOrf.txt | $EXE > wwwroot/https://github.com/Jeromerobichez/atelierDevops/blob/main/FlatBiblio/FlatBiblio.csproj
+cat data/_liste.txt data/rt25TYuy0.txt | $EXE > wwwroot/https://github.com/Jeromerobichez/atelierDevops/blob/main/FlatBiblio/FlatBiblio.csproj
